2c57691257ce9ee733bb373d0220358dc7e7cd8e
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-09-29 20:44:27 +0200

    functions

---
+++ Moduli_4/builtInFunctions.php
@@ -0,0 +1,14 @@
+<?php 
+     #phpinfo();
+     $x = 'Hello';
+     print_r($x);
+
+     echo '<br>';
+
+     $a = 5;
+     echo gettype($a) . '<br>';
+     $y = 10.3;
+     echo gettype($y) . '<br>';
+     $z = 'Hello';
+     echo gettype($z) . '<br>';
+?>
